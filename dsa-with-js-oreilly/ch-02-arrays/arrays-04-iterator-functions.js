3e0c0efcd4165d06f93d39966fa44246a73f0b35
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-05-24 12:12:14 +0530

    dsa-with-js-oreilly - 02-arrays - iterators-map and filter.

--- dsa-with-js-oreilly/ch-02-arrays/arrays-04-iterator-functions.js
+++ dsa-with-js-oreilly/ch-02-arrays/arrays-04-iterator-functions.js
@@ -15,6 +15,10 @@
     return num % 2 === 0
   }
 
+  function isOdd(num) {
+    return num % 2 !== 0
+  }
+
   let nums2 = [2, 4, 6, 8, 10, 5]
   let even = nums2.every(isEven)
 
@@ -70,6 +74,66 @@
   /*
    * iterator functions that return a new array
    * */
-  // 
+  // map()
+  function curve(grade) {
+    return grade += 5
+  }
+
+  const grades = [77, 65, 81, 92, 83]
+  let new_grades = grades.map(curve)
+
+  console.log('new grades', new_grades)
+
+  let words = [
+    'For',
+    'Your',
+    'Information',
+  ]
+  let acronym = words.map(word => word[0])
+
+  console.log(acronym.join(''))
+
+  // filter()
+  let nums5 = []
+
+  for (let i = 0; i < 20; ++i) {
+    nums5[i] = i + 1
+  }
+
+  let evenNums = nums5.filter(isEven)
+  let oddNums = nums5.filter(isOdd)
+
+  console.log('Even numbers:', evenNums)
+  console.log('Odd numbers:', oddNums)
+
+  let random_grades = []
+
+  for (let i = 0; i < 20; ++i) {
+    random_grades[i] = Math.floor(Math.random() * 101)
+  }
+
+  let pass_grades = random_grades.filter(num => num >= 60)
+
+  console.log('All grades:', random_grades)
+  console.log('Passing grades:', pass_grades)
+
+  // i before e except after c
+  var rule_words = [
+    'recieve',
+    'deceive',
+    'percieve',
+    'deceit',
+    'concieve',
+  ]
+  var misspelled = rule_words.filter(str => {
+    if (str.indexOf('cie') > -1) {
+      return true
+    }
+
+    return false
+  })
+
+  console.log('misspelled', misspelled)
+
 })()
 
