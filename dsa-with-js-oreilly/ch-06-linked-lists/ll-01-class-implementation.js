46e17e0448150d8905eb3475aa0b208264d9b5d1
ANUJ UPADHYAY <openmindedlearning@gmail.com>
2023-07-05 23:30:07 +0530

    Dsa with js oreilly (#18)
    * dsa-with-js-oreilly - continued debugging list implementation.
    
    * dsa-with-js-oreilly - stacks - stacks examples done.
    
    * dsa-with-js-oreilly - 05-queues - class definition.
    
    * dsa-with-js-oreilly - 05-queues - implementation program.
    
    * dsa-with-js-oreilly - 05-queues - priority queues.
    
    * dsa-with-js-oreilly - 05 Queues - priority queues implementation and deque.
    
    * dsa-with-js-oreilly - 02 Arrays - exercises.
    
    * dsa-with-js-oreilly - 02-arrays - exercises.
    
    * dsa-with-js-oreilly - 03 arrays - exercises finished.
    
    * dsa-with-js-oreilly - 06 linked lists - class implementation.
    
    ---------
    
    Co-authored-by: anuj <anuj@C40LAP-10090.india.rsystems.com>

---
+++ dsa-with-js-oreilly/ch-06-linked-lists/ll-01-class-implementation.js
@@ -0,0 +1,116 @@
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  append(newElement) {
+    const newNode = new Node(newElement)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+  }
+
+  prepend(item) {
+    const newNode = new Node(item)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.next = this.head
+      this.head = newNode
+    }
+  }
+
+  findPrevious(item) {
+    if (!this.head) {
+      return null
+    }
+
+    let currentNode = this.head
+    let previousNode = null
+
+    while (currentNode) {
+      if (currentNode.element === item) {
+        return previousNode
+      }
+
+      previousNode = currentNode
+      currentNode = currentNode.next
+    }
+
+    return null
+  }
+
+  display() {
+    const elements = []
+
+    let currentNode = this.head
+
+    while (currentNode) {
+      elements.push(currentNode.element)
+      currentNode = currentNode.next
+    }
+
+    console.log(elements.join(' -> '))
+  }
+
+  remove(item) {
+    if (!this.head) {
+      return
+    }
+
+    if (this.head.element === item) {
+      this.head = this.head.next
+
+      if (!this.head) {
+        this.tail = null
+      }
+
+      return
+    }
+
+    let currentNode = this.head
+
+    while (currentNode.next) {
+      if (currentNode.next.element === item) {
+        currentNode.next = currentNode.next.next
+
+        if (currentNode.next === null) {
+          this.tail = currentNode
+        }
+
+        return
+      }
+
+      currentNode = currentNode.next
+    }
+  }
+}
+
+const linkedList = new LinkedList()
+
+linkedList.append(5)
+linkedList.append(10)
+linkedList.append(15)
+linkedList.prepend(2)
+linkedList.remove(5)
+
+const previousNode = linkedList.findPrevious(15)
+
+console.log(`Previous Node: ${previousNode ? previousNode.element : 'not found!'}`)
+linkedList.display()
+
