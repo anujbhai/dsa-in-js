9eae25a7118ffaf87694a5553a5c5786bed029bb
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-08-08 23:31:04 +0530

    dsa-ztm - 02 hash tables - hash table implementation with keys.

---
+++ dsa-ztm/section-02-ds-hash-tables/hash-tables-01-class-implementation.js
@@ -0,0 +1,69 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size)
+  }
+
+  _hash(key) {
+    let hash = 0
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+
+    return hash
+  }
+
+  set(key, value) {
+    let pos = this._hash(key)
+
+    if (!this.data[pos]) {
+      this.data[pos] = []
+    }
+
+    this.data[pos].push([key, value])
+    console.log(this.data)
+
+    return this.data
+  }
+
+  get(key) {
+    let pos = this._hash(key)
+
+    const current = this.data[pos]
+
+    if (current) {
+      for (let i = 0; i < current.length; i++) {
+        if (current[i][0] === key) {
+          console.log(current[i][1])
+          return current[i][1]
+        }
+      }
+    }
+
+    return undefined
+  }
+
+  // iterate through all items
+  keys() {
+    const keysArr = []
+
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArr.push(this.data[i][0][0])
+      }
+    }
+
+    console.log('keys array:', keysArr)
+
+    return keysArr
+  }
+}
+
+const hTable = new HashTable(50)
+
+hTable.set('grapes', 10000)
+hTable.set('apples', 52)
+hTable.set('oranges', 2)
+hTable.get('apples')
+hTable.keys()
+
