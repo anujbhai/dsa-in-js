6d1fe16a15d8e461cbdd1f406594bbc7b78dffbe
ANUJ UPADHYAY <openmindedlearning@gmail.com>
2023-08-13 14:15:12 +0530

    Dsa with js oreilly (#31)
    dsa-with-js-oreilly - 08 Hashing - changes in storing and retreiving data.

--- dsa-with-js-oreilly/ch-08-hashing/hash-04-storing-retreiving-data.js
+++ dsa-with-js-oreilly/ch-08-hashing/hash-04-storing-retreiving-data.js
@@ -1,5 +1,5 @@
 const readline = require('readline')
-const rl = readline.createInterface(process.stdin, process.stdout)
+// const rl = readline.createInterface(process.stdin, process.stdout)
 
 class HashTable {
   constructor() {
@@ -44,29 +44,54 @@ class HashTable {
   }
 }
 
-const phNums = new HashTable()
-let name = ''
-let phNum = ''
+function getUserInput(prompt) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
 
-for (var i = 0; i < 3; i++) {
-  name = rl.question('Enter a name (space to quit): ')
-  phNum = rl.question('Enter a number: ')
+  return new Promise((resolve) => {
+    rl.question(prompt, (answer) => {
+      rl.close()
+
+      resolve(answer)
+    })
+  })
 }
 
-name = ''
+async function main() {
+  const hTable = new HashTable()
 
-while (name !== 'quit') {
-  // @TODO - assign typed name as prompt input
-  name = rl.question('Name for number (Enter <quit> to stop): ')
+  for (let i = 0; i < 3; i++) {
+    const name = await getUserInput(`Enter name ${i + 1}: `)
+    const phNumber = await getUserInput(`Enter phone number for ${name}: `)
 
-  if (name = 'quit') {
-    break;
+    hTable.put(name, phNumber)
   }
 
-  console.log(`${name}'s number is ${phNums.get(name)}`)
-  rl.question('Name of number (Enter <quit> to stop): ')
-  // rl.close()
+  console.log('\nData stored in hash table')
+  hTable.showDistro()
+
+  let retrieveName;
+
+  while (retrieveName !== 'quit') {
+    retrieveName = await getUserInput('/nEnter a name to retrieve the phone number (or <quit> to exit)')
+
+    if (retrieveName !== 'quit') {
+      const phNumber = hTable.get(retrieveName)
+
+      if (phNumber !== undefined) {
+        console.log(`Phone number for ${retrieveName}: ${phNumber}`)
+      } else {
+        console.log(`No phone number found for ${retrieveName}.`)
+      }
+    }
+  }
+
+  console.log('Goodbye!')
 }
+
+main()
 
 
 
