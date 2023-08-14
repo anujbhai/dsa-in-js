458877f938a0b20c3aed19729a0ba027e76755c6
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-08-14 21:28:46 +0530

    dsa-ztm - 04 stacks and queues - finished.

--- dsa-ztm/section-03-ds-linked-lists/linked-lists-01-class-implementation.js
+++ dsa-ztm/section-03-ds-linked-lists/linked-lists-01-class-implementation.js
@@ -94,6 +94,30 @@ class LinkedList {
     leader.next = nodeToRemove.next
     this.length--
 
+    // return this.display()
+  }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head
+    }
+
+    let first = this.head
+    this.tail = this.head
+
+    let second = first.next
+
+    while (second) {
+      const temp = second.next
+
+      second.next = first
+      first = second
+      second = temp
+    }
+
+    this.head.next = null
+    this.head = first
+
     return this.display()
   }
 
