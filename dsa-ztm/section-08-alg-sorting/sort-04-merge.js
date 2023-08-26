32c5e786b702956b888cdb333204cc54ee55ee8a
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-08-26 22:57:25 +0530

    dsa-ztm - 08 sorting algs - merge sort.

---
+++ dsa-ztm/section-08-alg-sorting/sort-04-merge.js
@@ -0,0 +1,45 @@
+const nums = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr
+  }
+
+  // split array into right and left
+  const length = arr.length
+  const middle = Math.floor(length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+
+  console.log('left: ', left)
+  console.log('right: ', right)
+
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right) {
+  const result = []
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex])
+      leftIndex++
+    } else {
+      result.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+
+  return result
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex))
+}
+
+const answer = mergeSort(nums)
+console.log(answer)
+
